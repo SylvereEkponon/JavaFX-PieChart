fac5c44f8a1a6b162837873af665053f88cc6a77
Sylvere Ekponon <tekponon@my.centennialcollege.ca>
2017-11-13 21:00:12 -0500

    Added event handler

--- src/application/MainController.java
+++ src/application/MainController.java
@@ -3,15 +3,19 @@ package application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.event.Event;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.chart.PieChart;
 import javafx.scene.chart.PieChart.Data;
+import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 
 public class MainController {
 	
 	@FXML PieChart piechart;
-	
-	
+	@FXML Label lblStatus;
+		
 	public void btn(ActionEvent event) {
 		
 		ObservableList<Data> list = FXCollections.observableArrayList(
@@ -22,6 +26,18 @@ public class MainController {
 				new PieChart.Data("C",15)
 				);
 		piechart.setData(list);
+		
+		for(final PieChart.Data data : piechart.getData()) 
+		{
+			data.getNode().addEventHandler(MouseEvent.MOUSE_CLICKED, new EventHandler<MouseEvent>() {
+
+				@Override
+				public void handle(MouseEvent event) {
+					
+					lblStatus.setText(String.valueOf(data.getPieValue()) + "%");
+				}
+			});
+		}
 	}
 
 }
